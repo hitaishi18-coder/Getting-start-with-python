1ac2d81ed5eb1b4a4a4d362c709eebad08e0fc03
hitaishi18-coder <hitaishi.8.8.8@gmail.com>
2025-12-31 17:31:27 +0530

    Fixed file extensions

--- 15_dictionary.py
+++ 15_dictionary.py
@@ -1,38 +1,53 @@
 thisdict = {
-    "brand" : "ford",
-    "model" : "mustang",
-    "year"  : 1876
+    "brand": "ford",
+    "model": "mustang",
+    "year": 1876
 }
+# Dictionary with key : value pairs
+# Keys are strings, values can be any data type
 
 print(thisdict)
+# Prints the complete dictionary
+
 print(thisdict["brand"])
+# Accessing value using key
+# Output: ford
 
-# changeable 
 
-# duplicates not allowed 
+# changeable
+# Dictionaries are mutable (values can be changed)
 
-#Duplicate values will overwrite existing values
+# duplicates not allowed
+# Duplicate keys will overwrite existing values
+
 thisdict = {
-  "brand": "Ford",
-  "model": "Mustang",
-  "year": 1964,
-  "year": 2020
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964,
+    "year": 2020
 }
+# The key "year" is repeated
+# The last value (2020) overwrites the previous one
+
 print(thisdict)
+
 
 print(len(thisdict))
-
+# len() returns number of key-value pairs in dictionary
 
 
 thisdict = {
-  "brand": "Ford",
-  "electric": False,
-  "year": 1964,
-  "colors": ["red", "white", "blue"]
+    "brand": "Ford",
+    "electric": False,
+    "year": 1964,
+    "colors": ["red", "white", "blue"]
 }
+# Dictionary can store different data types:
+# string, boolean, integer, list
 
 print(thisdict)
 
 
-thisdict = dict(name = "John", age = 36, country = "Norway")
+thisdict = dict(name="John", age=36, country="Norway")
+# Creating dictionary using dict() constructor
 print(thisdict)
